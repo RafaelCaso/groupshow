4ee2397354c1c7a0eeec37fc729379e5712c9cf5
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2023-05-17 14:11:56 -0600

    add prototype registrar

--- src/main/java/com/groupshow/GroupShowAppApplication.java
+++ src/main/java/com/groupshow/GroupShowAppApplication.java
@@ -1,13 +1,25 @@
 package com.groupshow;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.groupshow.utilities.Registrar;
 
 @SpringBootApplication
 public class GroupShowAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupShowAppApplication.class, args);
+		
+		Registrar registrar = new Registrar();
+		try {
+			registrar.sendEmail();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
