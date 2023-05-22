57eed612c05a3d5f4bf6b910d0a43ef4083b6fb1
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2023-05-22 15:09:13 -0600

    add JUnit test for TestUploadVideo, writing, song, and painting

--- src/test/java/com/groupshow/controllers/TestUploadSong.java
+++ src/test/java/com/groupshow/controllers/TestUploadSong.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.groupshow.models.Song;
+import com.groupshow.models.User;
+import com.groupshow.models.UserType;
+import com.groupshow.services.UserService;
 import com.groupshow.utilities.dto.SongDto;
 
 @SpringBootTest
@@ -15,17 +18,35 @@ public class TestUploadSong {
 
     @Autowired
     private SongController songController;
+    
+    @Autowired
+    private UserService userService;
 
     @Test
     public void testUploadSong() {
         SongDto song = new SongDto();
 
+        User user = new User();
+        
+        user.setUserID(1);
+        user.setEmail("adgadgqg");
+        user.setFirstName("Asgasg");
+        user.setLastName("asdgadsg");
+        user.setGradeLevel("qaegag");
+        user.setMajor("asfg");
+        user.setMinor("asgasg");
+        user.setUserType(UserType.STUDENT);
+        
+        
+        userService.addUser(user);
+        
         song.setArtworkTitle("TestSong");
         song.setArtistStatement("TestStatement");
         song.setArtworkURL("TestURL");
         song.setDurationMin(2);
         song.setDurationSec(38);
-
+        song.setArtistID(1);
+        
         Song savedSong = songController.uploadSong(song);
 
 
