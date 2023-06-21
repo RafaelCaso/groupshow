684fe2fc3aac13f8801140c981c4c1fddd37db98
Mark Satin <msatin2009@gmail.com>
2023-06-21 16:11:45 -0400

    deleted white space

--- src/main/java/com/groupshow/GroupShowAppApplication.java
+++ src/main/java/com/groupshow/GroupShowAppApplication.java
@@ -3,9 +3,9 @@ package com.groupshow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class GroupShowAppApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupShowAppApplication.class, args);
