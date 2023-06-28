087208db79057b0833970001597359244b01e61b
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2023-06-28 10:52:28 -0600

    uncomment @CrossOrigin

--- src/main/java/com/groupshow/artwork/ArtworkController.java
+++ src/main/java/com/groupshow/artwork/ArtworkController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/artwork")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 public class ArtworkController {
 
     @Autowired
